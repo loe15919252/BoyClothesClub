4819988aaacac0433a6139081f64671aceb22510
loe15919252 <you@example.com>
2016-05-25 19:34:28 +0800

    1.0.3 首页布局

--- app/src/main/java/com/men/boyclothesclub/FristPage1/FristPageFragment.java
+++ app/src/main/java/com/men/boyclothesclub/FristPage1/FristPageFragment.java
@@ -1,5 +1,6 @@
 package com.men.boyclothesclub.FristPage1;
 
+import android.media.Image;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.view.ViewPager;
@@ -14,6 +15,8 @@ import com.men.boyclothesclub.Base.adapter.ViewPagerCommonAdapter;
 import com.men.boyclothesclub.Base.ui.BaseFragment;
 import com.men.boyclothesclub.Base.utils.LogUtil;
 import com.men.boyclothesclub.Base.utils.OkHttpUtils;
+import com.men.boyclothesclub.FristPage1.bean.OtherBean;
+import com.men.boyclothesclub.FristPage1.bean.ShopBean;
 import com.men.boyclothesclub.FristPage1.bean.TagBean;
 import com.men.boyclothesclub.FristPage1.bean.ViewPagerBean;
 import com.men.boyclothesclub.FristPage1.utils.FristConstont;
@@ -43,21 +46,39 @@ public class FristPageFragment extends BaseFragment {
     private ViewPagerCommonAdapter viewPagerAdapter;
     private List<ImageView> list;
     private List<ViewPagerBean.DataBean> listBean;
-    private int indexVp;
+    //时间
     private Timer timer;
+    private int indexVp;
     private TimerTask timerTask;
-    private int topRes[] = new int[]{R.id.id_iv_top1,R.id.id_iv_top2,R.id.id_iv_top3,R.id.id_iv_top4};
-    private int topTVRes[] = new int[]{R.id.id_tv_top1,R.id.id_tv_top2,R.id.id_tv_top3,R.id.id_tv_top4};
+    //顶部图标
+    private int topRes[] = new int[]{R.id.id_iv_top1, R.id.id_iv_top2, R.id.id_iv_top3, R.id.id_iv_top4};
+    private int topTVRes[] = new int[]{R.id.id_tv_top1, R.id.id_tv_top2, R.id.id_tv_top3, R.id.id_tv_top4};
+    //品牌
+    private List<ImageView> ivpps;
+    private int ppRes[] = new int[]{R.id.id_iv_pp11, R.id.id_iv_pp12, R.id.id_iv_pp13, R.id.id_iv_pp14,
+            R.id.id_iv_pp21, R.id.id_iv_pp22};
+    //学堂
+    private ImageView ivxt1, ivxt2;
+    private TextView tvxt1, tvxt2, tvxtpl1, tvxtpl2, tvxtrd1, tvxtrd2;
+    //搭配
+    private TextView tvdp1, tvdpmore, tvdp_1, tvdp_2, tvdp_3, tvdp2, tvdp_21, tvdp_22, tvdp_23;
+    private ImageView ivdpbig1, ivdp11, ivdp12, ivdp13, ivdpbig2, ivdp21, ivdp22, ivdp23;
+    private TextView[] tvdps1, tvdps2;
+    private ImageView[] ivdps1, ivdps2;
+    //市场
+    private ImageView ivsp1, ivsp2, ivsp3, ivsp4;
+    private ImageView[] ivsps;
+    //
     public Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
-                case 1:
+                case FristConstont.HANDER_TAG_AD_TIMER:
                     indexVp %= 3;
                     viewPager.setCurrentItem(indexVp);
                     indexVp++;
                     break;
-                case 666:
+                case FristConstont.HANDER_TAG_AD:
                     String url = msg.obj.toString();
                     ViewPagerBean bean = ViewPagerBean.objectFromData(url);
                     listBean.addAll(bean.getData());
@@ -80,23 +101,39 @@ public class FristPageFragment extends BaseFragment {
                             (list, getActivity(), listBean);
                     viewPager.setAdapter(viewPagerAdapter);
                     break;
-                case 888:
+                case FristConstont.HANDER_TAG_TAG:
                     TagBean t = (TagBean) msg.obj;
                     for (int i = 0; i < 4; i++) {
                         try {
                             String urlImg = t.getData().get(i).getTheme_image();
                             ImageView iv = (ImageView) ll.findViewById(topRes[i]);
                             TextView tv = (TextView) ll.findViewById(topTVRes[i]);
-                            OkHttpUtils.setImage(urlImg,iv);
+                            OkHttpUtils.setImage(urlImg, iv);
                             tv.setText(t.getData().get(i).getTheme_name());
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
                     break;
+                case FristConstont.HANDER_TAG_OTHER:
+                    String url1 = msg.obj.toString();
+                    OtherBean other = OtherBean.objectFromData(url1);
+                    setSchoolView(other);
+                    setBrandView(other);
+                    setMatch(other);
+                    break;
+                case FristConstont.HANDER_TAG_SHOP:
+                    String url2 = msg.obj.toString();
+                    ShopBean shop = ShopBean.objectFromData(url2);
+                    for (int i = 0; i < ivsps.length; i++) {
+                        Picasso.with(getActivity()).load(shop.getData().get(i).getTheme_image())
+                                .into(ivsps[i]);
+                    }
+                    break;
             }
         }
     };
+
     private ViewGroup.LayoutParams lp;
 
     @Override
@@ -114,21 +151,149 @@ public class FristPageFragment extends BaseFragment {
         lv = (ListView) getRootLayout().findViewById(R.id.id_lv_frist);
         ll = (LinearLayout) inflater.inflate(R.layout.custom1_frist_top, null);
         lv.addHeaderView(ll);
-        setData();
+        lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        setTagIconData();
         setViewPager();
         setTime();
-        lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        setOther();
+        setShop();
     }
 
+
+    /**
+     * 定时器
+     */
     private void setTime() {
         timerTask = new TimerTask() {
             @Override
             public void run() {
-                handler.sendEmptyMessage(1);
+                handler.sendEmptyMessage(FristConstont.HANDER_TAG_AD_TIMER);
             }
         };
         timer = MainActivity.getTimer();
-        timer.schedule(timerTask, 2000, 1500);
+        timer.schedule(timerTask, 3000, 1500);
+    }
+
+    private void setOther() {
+
+        initBrandView();
+        initSchoolView();
+        initMatch();
+        OkHttpUtils.getRequest(FristConstont.URL_OTHER, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                LogUtil.e("其他请求出错");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                sendOKMessage(response, FristConstont.HANDER_TAG_OTHER);
+            }
+        });
+
+    }
+
+    private void initSchoolView() {
+        ivxt1 = (ImageView) ll.findViewById(R.id.id_iv_xt);
+        ivxt2 = (ImageView) ll.findViewById(R.id.id_iv_xt2);
+        tvxt1 = (TextView) ll.findViewById(R.id.id_tv_xt);
+        tvxt2 = (TextView) ll.findViewById(R.id.id_tv_xt2);
+        tvxtpl1 = (TextView) ll.findViewById(R.id.id_tv_xt_pl);
+        tvxtpl2 = (TextView) ll.findViewById(R.id.id_tv_xt_pl2);
+        tvxtrd1 = (TextView) ll.findViewById(R.id.id_tv_xt_rd);
+        tvxtrd2 = (TextView) ll.findViewById(R.id.id_tv_xt_rd2);
+    }
+
+    private void setSchoolView(OtherBean other) {
+        for (int i = 0; i < ppRes.length; i++) {
+            String ppurl = other.getData().getBrand().get(i).getBrandIcon();
+            Picasso.with(getActivity()).load(ppurl).into(ivpps.get(i));
+        }
+        Picasso.with(getActivity()).load(other.getData().getSchool().get(0).getImage())
+                .into(ivxt1);
+        Picasso.with(getActivity()).load(other.getData().getSchool().get(1).getImage())
+                .into(ivxt2);
+    }
+
+    private void initBrandView() {
+        ivpps = new ArrayList<>();
+        for (int i = 0; i < ppRes.length; i++) {
+            ImageView iv = new ImageView(getActivity());
+            iv = (ImageView) ll.findViewById(ppRes[i]);
+            ivpps.add(iv);
+        }
+    }
+
+    private void setBrandView(OtherBean other) {
+        tvxt1.setText(other.getData().getSchool().get(0).getTitle());
+        tvxt2.setText(other.getData().getSchool().get(1).getTitle());
+        tvxtpl1.setText(other.getData().getSchool().get(0).getClickCount() + "");
+        tvxtpl2.setText(other.getData().getSchool().get(1).getClickCount() + "");
+    }
+
+    private void initMatch() {
+        ivdpbig1 = (ImageView) ll.findViewById(R.id.id_iv_dp_big);
+        ivdpbig2 = (ImageView) ll.findViewById(R.id.id_iv_dp_big2);
+        ivdp11 = (ImageView) ll.findViewById(R.id.id_iv_dp_1);
+        ivdp12 = (ImageView) ll.findViewById(R.id.id_iv_dp_2);
+        ivdp13 = (ImageView) ll.findViewById(R.id.id_iv_dp_3);
+        ivdp21 = (ImageView) ll.findViewById(R.id.id_iv_dp_21);
+        ivdp22 = (ImageView) ll.findViewById(R.id.id_iv_dp_22);
+        ivdp23 = (ImageView) ll.findViewById(R.id.id_iv_dp_23);
+        ivdps1 = new ImageView[]{ivdp11, ivdp12, ivdp13};
+        ivdps2 = new ImageView[]{ivdp21, ivdp22, ivdp23};
+
+        tvdpmore = (TextView) ll.findViewById(R.id.id_tv_dp_more);
+        tvdp1 = (TextView) ll.findViewById(R.id.id_tv_dp1);
+        tvdp_1 = (TextView) ll.findViewById(R.id.id_tv_dp_1);
+        tvdp_2 = (TextView) ll.findViewById(R.id.id_tv_dp_2);
+        tvdp_3 = (TextView) ll.findViewById(R.id.id_tv_dp_3);
+        tvdp2 = (TextView) ll.findViewById(R.id.id_tv_dp2);
+        tvdp_21 = (TextView) ll.findViewById(R.id.id_tv_dp_21);
+        tvdp_22 = (TextView) ll.findViewById(R.id.id_tv_dp_22);
+        tvdp_23 = (TextView) ll.findViewById(R.id.id_tv_dp_23);
+        tvdps1 = new TextView[]{tvdp_1, tvdp_2, tvdp_3};
+        tvdps2 = new TextView[]{tvdp_21, tvdp_22, tvdp_23};
+    }
+
+    private void setMatch(OtherBean other) {
+        for (int i = 0; i < ivdps1.length; i++) {
+            String tagName = other.getData().getMatch().get(0).getTags().get(i).getTagName();
+            tvdps1[i].setText(tagName);
+            String itemurl = other.getData().getMatch().get(0).getSingleItems().get(i).getPic_url();
+            Picasso.with(getActivity()).load(itemurl).into(ivdps1[i]);
+        }
+        for (int i = 0; i < ivdps1.length; i++) {
+            String tagName = other.getData().getMatch().get(1).getTags().get(i).getTagName();
+            tvdps2[i].setText(tagName);
+            String itemurl = other.getData().getMatch().get(1).getSingleItems().get(i).getPic_url();
+            Picasso.with(getActivity()).load(itemurl).into(ivdps2[i]);
+        }
+        tvdp1.setText(other.getData().getMatch().get(0).getTheme_desc());
+        tvdp2.setText(other.getData().getMatch().get(1).getTheme_desc());
+        Picasso.with(getActivity()).load(other.getData().getMatch().get(0)
+                .getBig_image()).into(ivdpbig1);
+        Picasso.with(getActivity()).load(other.getData().getMatch().get(1)
+                .getBig_image()).into(ivdpbig2);
+    }
+
+    private void setShop() {
+        ivsp1 = (ImageView) ll.findViewById(R.id.id_iv_ts1);
+        ivsp2 = (ImageView) ll.findViewById(R.id.id_iv_ts2);
+        ivsp3 = (ImageView) ll.findViewById(R.id.id_iv_ts3);
+        ivsp4 = (ImageView) ll.findViewById(R.id.id_iv_ts4);
+        ivsps = new ImageView[]{ivsp1, ivsp2, ivsp3, ivsp4};
+        OkHttpUtils.getRequest(FristConstont.URL_SHOP, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                LogUtil.e("特色市场请求出错");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                sendOKMessage(response, FristConstont.HANDER_TAG_SHOP);
+            }
+        });
     }
 
     private void setViewPager() {
@@ -143,24 +308,13 @@ public class FristPageFragment extends BaseFragment {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                ResponseBody body = response.body();
-                String url = body.string();
-                Message message = Message.obtain();
-                message.what = 666;
-                message.obj = url;
-
-                handler.sendMessage(message);
+                sendOKMessage(response, FristConstont.HANDER_TAG_AD);
             }
         });
 
     }
 
-    @Override
-    protected void initEvent() {
-
-    }
-
-    public void setData() {
+    public void setTagIconData() {
         OkHttpUtils.getRequest(FristConstont.URL_TOPICON, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -175,9 +329,23 @@ public class FristPageFragment extends BaseFragment {
                 TagBean bean = TagBean.objectFromData(url);
                 Message message = Message.obtain();
                 message.obj = bean;
-                message.what = 888;
+                message.what = FristConstont.HANDER_TAG_TAG;
                 handler.sendMessage(message);
             }
         });
+    }
+
+    public void sendOKMessage(Response response, int what) throws IOException {
+        ResponseBody body = response.body();
+        String url = body.string();
+        Message message = Message.obtain();
+        message.what = what;
+        message.obj = url;
+        handler.sendMessage(message);
+    }
+
+    @Override
+    protected void initEvent() {
+
     }
 }
